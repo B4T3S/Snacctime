1352e0e4bba8c4b01f5fe374279ec02ca7e2fc66
Bates <florian@cegledi.net>
2024-01-04 02:48:14 +0100

    Added items page

---
+++ src/app/items/items.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-items',
+  templateUrl: './items.page.html',
+  styleUrls: ['./items.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class ItemsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
