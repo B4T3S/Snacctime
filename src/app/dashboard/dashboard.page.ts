e62a822db6fc05956931dcdf959ef16ee92412d5
Bates <florian@cegledi.net>
2024-01-04 03:02:57 +0100

    Updated dashboard

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,20 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { Component, inject } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
+import { IonicModule } from '@ionic/angular'
+import { UserService } from '../services/user.service'
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule],
 })
-export class DashboardPage implements OnInit {
+export class DashboardPage {
+  userService = inject(UserService)
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor() {}
 }
