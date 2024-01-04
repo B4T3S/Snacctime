0f49db35f8d02225c91e361ace2801a69a016bd7
Bates <florian@cegledi.net>
2024-01-04 02:48:26 +0100

    Added route for items page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,27 @@
-import { Routes } from '@angular/router';
-import { IsLoggedInGuard } from './guards/is-logged-in.guard';
+import { Routes } from '@angular/router'
+import { IsLoggedInGuard } from './guards/is-logged-in.guard'
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full',
-  },
-  {
-    path: 'login',
-    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
-  },
-  {
-    path: 'dashboard',
-    loadComponent: () => import('./dashboard/dashboard.page').then( m => m.DashboardPage),
-    canActivate: [IsLoggedInGuard]
-  },
-
-
-];
+    {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+    },
+    {
+        path: 'login',
+        loadComponent: () =>
+            import('./login/login.page').then((m) => m.LoginPage),
+    },
+    {
+        path: 'dashboard',
+        loadComponent: () =>
+            import('./dashboard/dashboard.page').then((m) => m.DashboardPage),
+        canActivate: [IsLoggedInGuard],
+    },
+    {
+        path: 'items',
+        loadComponent: () =>
+            import('./items/items.page').then((m) => m.ItemsPage),
+        canActivate: [IsLoggedInGuard],
+    },
+]
