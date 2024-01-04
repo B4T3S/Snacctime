9f75c63d3290cd37a28c6ed3b2409153585dadce
Bates <florian@cegledi.net>
2024-01-04 03:02:23 +0100

    Made service injection cleaner

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,52 +1,53 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-import { UserService } from '../services/user.service';
-import { Router } from '@angular/router';
-import { LoadingController } from '@ionic/angular/standalone';
-import { ViewWillEnter } from '@ionic/angular/standalone';
+import { Component, OnInit, inject } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
+import { IonicModule } from '@ionic/angular'
+import { UserService } from '../services/user.service'
+import { Router } from '@angular/router'
+import { LoadingController } from '@ionic/angular/standalone'
+import { ViewWillEnter } from '@ionic/angular/standalone'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class LoginPage implements ViewWillEnter {
-  userService: UserService = inject(UserService);
-  router: Router = inject(Router);
-  loadingController: LoadingController = inject(LoadingController);
+  userService: UserService
+  router: Router
+  loadingController: LoadingController = inject(LoadingController)
 
-  username: string = "";
-  password: string = "";
-  errorText: string = "";
+  username: string = ''
+  password: string = ''
+  errorText: string = ''
 
-  constructor() { 
+  constructor() {
+    this.userService = inject(UserService)
+    this.router = inject(Router)
     if (this.userService.IsLoggedIn) {
-      this.router.navigate(['/dashboard']);
+      this.router.navigate(['/dashboard'])
     }
   }
 
   ionViewWillEnter(): void {
-    this.username = "";
-    this.password = "";
-    this.errorText = "";
+    this.username = ''
+    this.password = ''
+    this.errorText = ''
   }
 
   public async login() {
-    const loader = await this.loadingController.create();
-    loader.present();
+    const loader = await this.loadingController.create()
+    loader.present()
     try {
       if (await this.userService.login(this.username, this.password)) {
-        await this.router.navigate(['/dashboard']);
+        await this.router.navigate(['/dashboard'])
       }
     } catch (ex) {
-      this.errorText = "Login failed! Wrong username/password?";
+      this.errorText = 'Login failed! Wrong username/password?'
     } finally {
-      loader.dismiss();
+      loader.dismiss()
     }
   }
-
 }
