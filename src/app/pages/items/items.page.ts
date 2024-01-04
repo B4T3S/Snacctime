95d92bd2c71e80cdac091658a0cb866c02d260dd
Bates <florian@cegledi.net>
2024-01-04 03:05:47 +0100

    Moved pages into subfolder

---
+++ src/app/pages/items/items.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
+import { IonicModule } from '@ionic/angular'
+
+@Component({
+  selector: 'app-items',
+  templateUrl: './items.page.html',
+  styleUrls: ['./items.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule],
+})
+export class ItemsPage {
+  constructor() {}
+}
