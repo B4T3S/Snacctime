b95eeeeaa4cab22cf53aa93e11db995c4fe498ef
Bates <florian@cegledi.net>
2024-01-04 02:47:59 +0100

    Added global icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,69 @@
-import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { Router, RouterLink, RouterLinkActive } from '@angular/router';
-import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonAvatar, IonChip } from '@ionic/angular/standalone';
-import { addIcons } from 'ionicons';
-import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp, logOutSharp } from 'ionicons/icons';
-import { UserService } from './services/user.service';
+import { CommonModule } from '@angular/common'
+import { Component, inject } from '@angular/core'
+import { Router, RouterLink, RouterLinkActive } from '@angular/router'
+import {
+  IonApp,
+  IonSplitPane,
+  IonMenu,
+  IonContent,
+  IonList,
+  IonListHeader,
+  IonNote,
+  IonMenuToggle,
+  IonItem,
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonAvatar,
+  IonChip,
+} from '@ionic/angular/standalone'
+import { addIcons } from 'ionicons'
+import * as ionIcons from 'ionicons/icons'
+import { UserService } from './services/user.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, CommonModule, IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonAvatar, IonChip],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    CommonModule,
+    IonApp,
+    IonSplitPane,
+    IonMenu,
+    IonContent,
+    IonList,
+    IonListHeader,
+    IonNote,
+    IonMenuToggle,
+    IonItem,
+    IonIcon,
+    IonLabel,
+    IonRouterOutlet,
+    IonAvatar,
+    IonChip,
+  ],
 })
 export class AppComponent {
-  userService: UserService = inject(UserService);
-  router: Router = inject(Router);
+  userService: UserService = inject(UserService)
+  router: Router = inject(Router)
 
   public get isLoggedIn(): boolean {
-    return this.userService.IsLoggedIn;
+    return this.userService.IsLoggedIn
   }
 
   public get username(): string {
-    return this.userService.Username;
+    return this.userService.Username
   }
 
   public get avatar(): string {
-    return this.userService.Avatar;
+    return this.userService.Avatar
   }
 
   public async logout() {
-    this.userService.logout();
+    this.userService.logout()
     await this.router.navigate(['/login'])
   }
 
@@ -41,9 +74,9 @@ export class AppComponent {
     { title: 'Archived', url: '/folder/archived', icon: 'archive' },
     { title: 'Trash', url: '/folder/trash', icon: 'trash' },
     { title: 'Spam', url: '/folder/spam', icon: 'warning' },
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  ]
+  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders']
   constructor() {
-    addIcons({ mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp, logOutSharp });
+    addIcons(ionIcons)
   }
 }
